86dd4b24d186f70d18c8399b9b89e10eaea8cb09
xb1123 <14042305+xb1123xb1123@user.noreply.gitee.com>
2026-08-07 15:21:27 +0000

    update deploy.sh.
    Signed-off-by: xb1123 <14042305+xb1123xb1123@user.noreply.gitee.com>

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ npm install
 
 cd .. # 回到项目根目录
 chmod +x update-index.sh
-./update-index.sh             # 执行更新脚本
+bash ./update-index.sh             # 执行更新脚本
 cd haige                     # 返回 haige 目录
 
 npm run build
