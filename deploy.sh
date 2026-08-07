c858259aa138dc6e0a9d003a8427323d71f8a980
xb1123 <14042305+xb1123xb1123@user.noreply.gitee.com>
2026-08-07 15:05:36 +0000

    update deploy.sh.
    Signed-off-by: xb1123 <14042305+xb1123xb1123@user.noreply.gitee.com>

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 cd haige
-npm install docusaurus
+npm install
+
+cd ..                         # 回到项目根目录
+./update-index.sh             # 执行更新脚本
+cd haige                     # 返回 haige 目录
+
 npm run build
 cd ..
 cp -r haige/build/* ./
